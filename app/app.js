a6dbdff07f3808f1bb2d6f663013323d4135be68
Braun, Sierra <siebraun@iu.edu>
2022-11-12 09:30:05 -0500

    updates

--- app/app.js
+++ app/app.js
@@ -1,17 +1,43 @@
 import * as MODEL from "./model.js";
 
+// RECIPES DATA
+let obj = {
+  Recipes: [
+    {
+      name: "Supreme Pizza",
+      imgsrc: "../assets/images/recipe-pizza.jpg",
+      desc: "mmm yummy pizza",
+      totalTime: "1hr 24min",
+      servings: "4 servings",
+      ingred1: "ham",
+      ingred2: "burger",
+      ingred3: "",
+      instruct1: "go to mcdonalds",
+      instruct2: "order hamburger",
+      instruct3: "eat hamburger",
+    },
+  ],
+};
+// ROUTING
 function changeRoute() {
   let hashTag = window.location.hash;
   let pageID = hashTag.replace("#", "");
 
-  if (pageID == "" || pageID == "home") {
+  if (
+    pageID == "" ||
+    pageID == "home" ||
+    pageID == "createRecipe" ||
+    pageID == "editRecipe" ||
+    pageID == "viewRecipe"
+  ) {
     MODEL.changePage(pageID);
     $("#app, nav, footer").css("background-color", "transparent");
   } else if (pageID == "login") {
     MODEL.changePage(pageID);
     $("#app, nav, footer").css("background-color", "var(--yellow)");
-  } else {
+  } else if (pageID == "browse" || pageID == "yourRecipes") {
     MODEL.changePage(pageID);
+    $("#app, nav, footer").css("background-color", "var(--green)");
   }
 }
 
@@ -20,19 +46,22 @@ function initApp() {
   $(window).on("hashchange", changeRoute);
 }
 
-//log-in function
+// LOG IN FUNCTION
 export function initSubmitListener() {
-  $("#register").click(function (e) {
+  $(".register").click(function (e) {
     let email = $("#email").val();
     let password = $("#pw").val();
     let fname = $("#fname").val();
     let lname = $("#lname").val();
-    // console.log("inputs " + email + " " + password + " " + fname + " " + lname);
 
+    // show hidden nav on log-in
     $(".hidden").css("display", "inline");
     console.log("hi");
+
+    //navigate to your recipes page
+    MODEL.changePage("yourRecipes");
+    $("#app, nav, footer").css("background-color", "var(--green)");
   });
-  // console.log("hi");
 }
 
 $(document).ready(function () {
--- app/model.js
+++ app/model.js
@@ -4,12 +4,10 @@ var userInfo = {};
 export function changePage(pageID, subPageID) {
   if (subPageID == undefined) {
     $.get(`pages/${pageID}.html`, function (data) {
-      // console.log(data);
       $("#app").html(data);
       MODEL.initSubmitListener();
     }).fail((error) => {
       if (error.status == "404") {
-        //   alert("Page can not be found. Please check your url!");
       }
       console.log("error", error.status);
     });
