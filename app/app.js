3f75d7b6b42b2e51acab7bdc38e8cd9bd1f6639b
Braun, Sierra <siebraun@iu.edu>
2022-11-21 08:49:05 -0500

    editing login/create functions

--- app/app.js
+++ app/app.js
@@ -17,41 +17,13 @@ let obj = {
       instruct3: "eat hamburger",
     },
   ],
-
-  yourRecipes: [
-    {
-      name: "Supreme Pizza",
-      imgsrc: "../assets/images/recipe-pizza.jpg",
-      desc: "mmm yummy pizza",
-      totalTime: "1hr 24min",
-      servings: "4 servings",
-      ingred1: "ham",
-      ingred2: "burger",
-      ingred3: "",
-      instruct1: "go to mcdonalds",
-      instruct2: "order hamburger",
-      instruct3: "eat hamburger",
-    },
-  ],
 };
 
-// define localstorage items
-let yourimg = localStorage.getItem("imgsrc");
-let yourname = localStorage.getItem("name");
-let yourdesc = localStorage.getItem("desc");
-let yourtime = localStorage.getItem("totalTime");
-let yourservings = localStorage.getItem("servings");
-let youringred1 = localStorage.getItem("ingred1");
-let youringred2 = localStorage.getItem("ingred2");
-let youringred3 = localStorage.getItem("ingred3");
-let yourinst1 = localStorage.getItem("instruct1");
-let yourinst2 = localStorage.getItem("instruct2");
-let yourinst3 = localStorage.getItem("instruct3");
-
-window.localStorage.setItem("yourrecipes", JSON.stringify(obj.Recipes));
-
-let newRecipe = window.localStorage.getItem("yourrecipes");
-console.log(JSON.parse(newRecipe));
+let recipeObj = {
+  info: [],
+  ingredients: [],
+  instructions: [],
+};
 
 // ROUTING
 function changeRoute() {
@@ -91,6 +63,7 @@ export function initSubmitListener() {
     let password = $("#pw").val();
     let fname = $("#fname").val();
     let lname = $("#lname").val();
+    // let firstname = localStorage.getItem("fname");
 
     // show hidden nav on log-in
     $(".hidden").css("display", "inline");
@@ -98,14 +71,15 @@ export function initSubmitListener() {
 
     //navigate to your yourrecipes page
     MODEL.changePage("createRecipe");
-    $("#app, nav, footer").css("background-color", "white");
+    $("#app, nav, footer").css("background-color", "transparent");
 
-    $(".create-wrapper .formHolder").append(`<h1>Hi</h1>`);
+    console.log(fname);
+
+    $("").prepend("hello");
 
     // console.log("hi");
   });
 }
-
 //
 //
 //
@@ -144,58 +118,46 @@ export function loopData() {
   $(".recipes").html("");
 
   // add recipe to list
-  $.each(obj.yourRecipes, (idx, yourrecipes) => {
-    $(".yourrecipes").append(
-      `<p>${yourrecipes.name}</p>
-      <p>${yourrecipes.imgsrc}</p>
-      <p>${yourrecipes.desc}</p>
-      <p>${yourrecipes.totalTime}</p>
-      <p>${yourrecipes.servings}</p>
-      <p>${yourrecipes.ingred1}</p>
-      <p>${yourrecipes.ingred2}</p>
-      <p>${yourrecipes.ingred3}</p>
-      <p>${yourrecipes.instruct1}</p>
-      <p>${yourrecipes.instruct2}</p>
-      <p>${yourrecipes.instruct3}</p>
-      `
-    );
+  // $.each(obj.yourRecipes, (idx, yourrecipes) => {
+  //   $(".yourrecipes").append(
+  //     `<p>${yourrecipes.name}</p>
+  //     <p>${yourrecipes.imgsrc}</p>
+  //     <p>${yourrecipes.desc}</p>
+  //     <p>${yourrecipes.totalTime}</p>
+  //     <p>${yourrecipes.servings}</p>
+  //     <p>${yourrecipes.ingred1}</p>
+  //     <p>${yourrecipes.ingred2}</p>
+  //     <p>${yourrecipes.ingred3}</p>
+  //     <p>${yourrecipes.instruct1}</p>
+  //     <p>${yourrecipes.instruct2}</p>
+  //     <p>${yourrecipes.instruct3}</p>
+  //     `
+  //   );
 
-    localStorage.setItem("imgsrc", JSON.stringify(yourrecipes.imgsrc));
-    localStorage.setItem("name", JSON.stringify(yourrecipes.name));
-    localStorage.setItem("desc", yourrecipes.desc);
-    localStorage.setItem("totalTime", yourrecipes.totalTime);
-    localStorage.setItem("servings", yourrecipes.servings);
-    localStorage.setItem("ingred1", yourrecipes.ingred1);
-    localStorage.setItem("ingred2", yourrecipes.ingred2);
-    localStorage.setItem("ingred3", yourrecipes.ingred3);
-    localStorage.setItem("instruct1", yourrecipes.instruct1);
-    localStorage.setItem("instruct2", yourrecipes.instruct2);
-    localStorage.setItem("instruct3", yourrecipes.instruct3);
+  //   $(".recipes").append(`
+  // <div class="recipe-wrapper">
+  //     <div class="recipe-details">
+  //       <div class="recipe-img">
+  //         <button onclick="viewRecipe(${idx})">View</button>
+  //       </div>
 
-    $(".recipes").append(`
-  <div class="recipe-wrapper">
-      <div class="recipe-details">
-        <div class="recipe-img">
-          <button onclick="viewRecipe(${idx})">View</button>
-        </div>
+  //       <div class="recipe-info">
+  //         <h1>${yourname}</h1>
+  //         <p>${yourdesc}</p>
+  //         <p>${yourtime}</p>
+  //         <p>${yourservings}</p>
+  //       </div>
+  //     </div>
 
-        <div class="recipe-info">
-          <h1>${yourname}</h1>
-          <p>${yourdesc}</p>
-          <p>${yourtime}</p>
-          <p>${yourservings}</p>
-        </div>
-      </div>
+  //     <div class="recipe-buttons">
+  //       <button onclick="editRecipe(${idx})">Edit</button>
+  //       <button id="delete">Delete</button>
+  //     </div>
+  //   </div>
+  // `);
 
-      <div class="recipe-buttons">
-        <button onclick="editRecipe(${idx})">Edit</button>
-        <button id="delete">Delete</button>
-      </div>
-    </div>
-  `);
-
-    // viewRecipe();
-  });
+  //   // viewRecipe();
+  // });
 }
 
 export function deleteRecipe(recipeId) {
@@ -218,38 +180,65 @@ export function deleteRecipe(recipeId) {
 
 // create recipe
 export function createRecipe() {
-  // add recipe
-  $("#addRecipeSubmit").click(function (e) {
-    let rN = $("#recipeName").val();
-    let rImg = $("#recipeImg").val();
-    let rDesc = $("#recipeDesc").val();
-    let rTime = $("#recipeTime").val();
-    let rServings = $("#recipeImg").val();
-    let i1 = $("#ingred1").val();
-    let i2 = $("#ingred2").val();
-    let i3 = $("#ingred3").val();
-    let inst1 = $("#instruct1").val();
-    let inst2 = $("#instruct2").val();
-    let inst3 = $("#instruct3").val();
+  let ingredCount = 3;
+  let instCount = 3;
 
-    // add recipe to array
-    obj.yourRecipes.push({
-      name: rN,
-      imgsrc: rImg,
-      desc: rDesc,
-      totalTime: rTime,
-      servings: rServings,
-      ingred1: i1,
-      ingred2: i2,
-      ingred3: i3,
-      instruct1: inst1,
-      instruct2: inst2,
-      instruct3: inst3,
+  $("#addIngred").on("click", (e) => {
+    // console.log("hi!");
+
+    $(".formIngredients").append(
+      `<input id="ingred$(ingredCount)" placeholder="Ingredient # ${
+        ingredCount + 1
+      }" type="text">`
+    );
+
+    ingredCount++;
+    console.log(ingredCount);
+  });
+
+  // add instructions
+  $("#addInst").on("click", (e) => {
+    // console.log("hi!");
+
+    $(".formInstructions").append(
+      `<input id="inst$(instCount)" placeholder="Instruction #${
+        instCount + 1
+      }" type="text">`
+    );
+
+    instCount++;
+    console.log(instCount);
+  });
+
+  //
+  //
+  //
+  //
+  // submit the info
+  $("#recipeSubmit").on("click", (e) => {
+    e.preventDefault();
+
+    $(".formHolder .formInstructions input").each((idx, instruction) => {
+      //   console.log(instruction.value);
+      recipeObj.instructions.push({ instructions: instruction.value });
     });
 
-    console.log(obj.yourRecipes);
-    loopData();
-    // document.getElementById("addStudent").val("");
+    $(".formHolder .formIngredients input").each((idx, ingredients) => {
+      //   console.log(instruction.value);
+      recipeObj.ingredients.push({ ingredients: ingredients.value });
+    });
+
+    $(".formHolder .basicInfo input").each((idx, info) => {
+      recipeObj.info.push({
+        info: info.value,
+      });
+    });
+
+    console.log(recipeObj);
+
+    $(".recipes").html(recipeObj);
+
+    // $(".data").html(recipeObj);
   });
 }
 
